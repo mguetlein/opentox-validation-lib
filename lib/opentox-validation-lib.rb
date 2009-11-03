bceeda3d4fc7fa7b53aa09388a91241e8dabde59
Martin Gütlein <martin.guetlein@gmail.com>
2009-11-03 11:37:27 +0100

    check for R_HOME, import of rinruby

--- lib/opentox-validation-lib.rb
+++ lib/opentox-validation-lib.rb
@@ -1,5 +1,9 @@
 
+raise "Environment variable R_HOME missing" unless ENV['R_HOME']
+ENV['PATH'] = ENV['R_HOME']+":"+ENV['PATH']
+
 require 'opentox-ruby-api-wrapper'
+require 'rinruby'
 
 require 'test_util'
 require 'prediction_util'
